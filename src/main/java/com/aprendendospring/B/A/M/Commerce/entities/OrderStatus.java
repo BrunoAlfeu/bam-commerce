2757abc40dcd19091935ed07b19527bccb7b2ac2
BrunoAlfeu <brunoalfeu213@gmail.com>
2025-07-07 20:01:58 -0300

    feat(entities): cria entidades iniciais User e Order

---
+++ src/main/java/com/aprendendospring/B/A/M/Commerce/entities/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.aprendendospring.B.A.M.Commerce.entities;
+
+public enum OrderStatus {
+
+	WAITING_PAYMENT,
+	PAID,
+	SHIPPED,
+	DELIVERED,
+	CANCELED;
+}
---
+++ src/test/java/com/aprendendospring/B/A/M/Commerce/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendendospring.B.A.M.Commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
